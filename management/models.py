ddd2a301079abb1afe66d8337b96ef0128d474c3
Raiki Kudo <105319451+KudouRaiki1016@users.noreply.github.com>
2023-07-10 14:18:00 +0900

    update_Task_commit

--- management/models.py
+++ management/models.py
@@ -2,13 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    TYPE_CHOICES = (
+        ('text', 'テキスト'),
+        ('checkbox', 'チェックボックス'),
+        ('file', 'ファイル'),
+    )
     name = models.CharField(max_length=30) # タスク名
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # 作成者（タスクの）
-    text = models.TextField() # テキスト（作成者が入力するタスクに関するテキスト）
-    closing = models.DateTimeField() # 締切日時（作成者が設定する）
-    created_at = models.DateTimeField(auto_now_add=True) # 作成日時（作成時に自動設定される）
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # 作成者（講師）
+    task_type = models.CharField(max_length=20, choices=TYPE_CHOICES) # 課題の種類
+    closing = models.DateTimeField() # 締切日時（講師が設定）
+    created_at = models.DateTimeField(auto_now_add=True) # 作成日時（自動設定）
+    description = models.TextField() # 課題の説明（講師が入力）
+    # チェックボックス形式で提出させる場合
+    checkboxes = models.ManyToManyField('CheckboxOption', blank=True)
+
     def __str__(self):
         return self.name
+
+class CheckboxOption(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.label
+
 
 
 class Submission(models.Model):
